9acb65032c36ae3d9e6d88f39de03237276e9458
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-25 16:35:45 +0300

    Routes for login and new sessions, refactoring some code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  get 'sessions/new'
   root 'static_pages#home'
  
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  # Sessions
+
+  get 'sessions/new'
   resources :users
 
+  # Login 
+
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessons#destroy'
 
 end
